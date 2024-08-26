22361227679aa19e7e1a2fb3500c651c7f67eedd
Suraj Yadav <surajyadavy676@.com>
2024-08-26 10:34:54 +0530

    again change var

--- src/data/apolloClient.js
+++ src/data/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 let api = process.env.NEXT_PUBLIC_API_URL;
-// console.log("api : ",api)
+console.log("api : ",api)
 const client = new ApolloClient({
   uri: `${api}/graphql`,
   cache: new InMemoryCache(),
